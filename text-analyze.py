0812a04a83894aba6ff932eeae255b82817cd3b7
Ali Moallemi <alimoallemi30@gmail.com>
2024-06-21 18:21:21 +0330

    Prepare project for production

--- text-analyze.py
+++ text-analyze.py
@@ -139,4 +139,5 @@ def index():
     return render_template('index.html', results="", chart_url="")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
